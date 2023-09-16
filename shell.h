540ec86cfb826c7c5ecac37a6f4e4fe47957a69d
Aya11ali <alyzaghlol340@gmail.com>
2023-09-16 07:39:31 -0700

    header file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/limits.h>
+#include <dirent.h>
+
+
+#endif
